3c53e76609cbf0bdd61f03fbad975a2b557b89f4
David Hankin <d_hankin@outlook.com>
2024-10-16 15:32:41 +0200

    communnication with online server test

---
+++ src/login/Login.tsx
@@ -0,0 +1,109 @@
+import React, { FormEvent } from 'react'
+
+interface LoginProps {
+    modeUrl: string;
+    handleLoginSuccess: () => void;
+    handleUserObject: (user: User) => void;
+    }
+
+    interface User {
+        id: string; 
+        username: string;
+        password: string;
+        firstName: string;
+        lastName: string;
+        hikes: Hike[];
+        friends: string[]; 
+    }
+    
+    interface Hike {
+        name: string;
+        startLocation: {
+            latitude: number;
+            longitude: number;
+        };
+        finishLocation: {
+            latitude: number;
+            longitude: number;
+        };
+        startTime: string; 
+        finishTime: string; 
+        distance: number; 
+        duration: number; 
+        route: string; 
+        isFavorite: boolean;
+        avgHeartRate: number; 
+        avgTemp: number; 
+        alerts: Alert[];
+    }
+    
+    interface Alert {
+        alertType: string;
+        information: string;
+        time: string; 
+        location: {
+            latitude: number;
+            longitude: number;
+        };
+    }
+
+function Login(props: LoginProps) {
+
+    const [username, setUsername] = React.useState<string>('');
+    const [password, setPassword] = React.useState<string>('');
+
+    function handleSubmit(event: FormEvent<HTMLFormElement>): void {
+        event.preventDefault();
+        fetch(props.modeUrl + '/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password
+            }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Message:', data);
+            localStorage.setItem('token', data.token);
+            props.handleLoginSuccess();
+            props.handleUserObject(data.user);
+            
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+
+    }
+
+  return (
+    <>
+        <div>Log in</div>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Email</label>
+            <input 
+                type="text" 
+                id="username" 
+                name="username" 
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+            />
+            <label htmlFor="password">Password</label>
+            <input 
+                type="password" 
+                id="password" 
+                name="password" 
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+            />
+            <button type="submit">Log in</button>
+        </form>
+    </>
+  )
+}
+
+export default Login
